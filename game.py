03439317b63d377e56bf9f6a658aff26028faf54
ZWooden26 <m266984@usna.edu>
2023-10-12 10:26:17 -0400

    screen

--- game.py
+++ game.py
@@ -2,7 +2,19 @@ import pygame
 import sys
 
 pygame.init()
-width = 800
-height = 600
+WIDTH = 800
+HEIGHT = 600
+BLUE = (0, 0, 225)
+BROWN = (204, 129, 43)
 
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("CHOMP")
 
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill(BLUE)
+    pygame.draw.rect(screen, BROWN, (0, 500, WIDTH, 100))
+    pygame.display.flip()
